8e70753d7de8863e9f924a02de3f850fb99a7d20
Tyler Collier <tyler.collier+github@devmounta.in>
2018-06-27 12:13:28 -0700

    Through step 10

---
+++ server/graphql/schema.js
@@ -0,0 +1,60 @@
+const characters = require('./model')
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLBoolean,
+  GraphQLID,
+} = require('graphql')
+const axios = require('axios')
+
+const Movie = new GraphQLObjectType({
+  name: 'Movie',
+  fields: () => {
+    return {
+      title: { type: GraphQLString },
+      releaseDate: {
+        type: GraphQLString,
+        resolve: movie => movie.release_date,
+      },
+    }
+  }
+})
+
+const Person = new GraphQLObjectType({
+  name: 'Person',
+  fields: () => {
+    return {
+      id: { type: GraphQLInt },
+      name: { type: GraphQLString },
+      height: { type: GraphQLString },
+      films: {
+        type: GraphQLList(Movie),
+        resolve: (person) => {
+          return person.films.map(film => {
+            return axios.get(film).then(res => res.data)
+          })
+        }
+      }
+    }
+  }
+})
+
+const Query = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => {
+    return {
+      people: {
+        type: new GraphQLList(Person),
+        resolve: () => characters,
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: Query
+})
--- server/index.js
+++ server/index.js
@@ -2,10 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const PORT = 3050
+const graphqlHTTP = require('express-graphql')
+const schema = require('./graphql/schema')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(cors())
+
+// endpoint: /graphql
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true,
+}));
 
 app.listen(PORT, () => console.log('Listening on Port: ' + PORT))
