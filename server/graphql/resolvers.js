944f3d064eb330e4edb119ddefb4a56085b19476
Tyler Collier <tyler.collier+github@devmounta.in>
2018-11-27 08:11:09 -0700

    Add person query

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -5,6 +5,10 @@ module.exports = {
   Query: {
     people: () => characters.map(x => {
       return {...x, homeWorld: axios.get(x.homeworld).then(response => response.data)}
-    })
+    }),
+    person: (parent, args) => {
+      const p = characters.find(x => x.id === args.id);
+      return { ...p, homeWorld: axios.get(p.homeworld).then(response => response.data)}
+    }
   }
 }
