45a4a17b2954ab128eb62682ff5139867a712173
Tyler Collier <tyler.collier+github@devmounta.in>
2018-11-27 08:07:10 -0700

    Add homeWorld

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,7 +1,10 @@
 const characters = require('./model');
+const axios = require('axios');
 
 module.exports = {
   Query: {
-    people: () => characters
+    people: () => characters.map(x => {
+      return {...x, homeWorld: axios.get(x.homeworld).then(response => response.data)}
+    })
   }
 }
